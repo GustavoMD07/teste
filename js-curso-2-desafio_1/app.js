6382dd93e3f15649718a207f53194ac5830d3705
Gustavo <gustavomatachun.domingues@gmail.com>
2024-05-22 18:06:50 -0300

    Update app.js

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -6,7 +6,7 @@ linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
 
 console.log(linguagensDeProgramacao);
 
-let listaDeNomes = ["Pedro", "Gustavo", "Clara"];
+let listaDeNomes = ["Pedro", "Gustavo", "Gabi"];
 console.log(listaDeNomes[0]);
 console.log(listaDeNomes[1]);
 console.log(listaDeNomes[listaDeNomes.length - 1])
