bb4647109e1c3e3f0e10d7c760d5e5e34befde0c
Gustavo <gustavomatachun.domingues@gmail.com>
2024-05-22 18:00:07 -0300

    Add files via upload

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,12 @@
+let listaGenerica = []
+
+let linguagensDeProgramacao = ["JavaScript","C","C++", "Kotlin", "Python"];
+
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+console.log(linguagensDeProgramacao);
+
+let listaDeNomes = ["Pedro", "Gustavo", "Clara"];
+console.log(listaDeNomes[0]);
+console.log(listaDeNomes[1]);
+console.log(listaDeNomes[listaDeNomes.length - 1])
